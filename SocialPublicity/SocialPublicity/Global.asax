069fa528826113d91656a5a22c1d19859d09b4d6
Manuel Villanueva <1mm.villanueva1@gmail.com>
2017-02-11 21:04:02 -0600

    creacion de proyecto.

---
+++ SocialPublicity/SocialPublicity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialPublicity.MvcApplication" Language="C#" %>
---
+++ SocialPublicity/SocialPublicity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialPublicity.Startup))]
+namespace SocialPublicity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
